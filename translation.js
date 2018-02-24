b316bbc12b6ace1c0f0292fc47343acf583ede2f
David Edler <david.edler@researchgate.net>
2018-02-24 12:26:27 +0100

    translated manual

--- translation.js
+++ translation.js
@@ -68,5 +68,32 @@ var TRANSLATION_MAP = {
   },
   "punkte":{
     "english":"points"
+  },
+  "Legen des ersten Wortes":{
+    "english":"Inserting the first word"
+  },
+  "Der erste Spieler bildet ein Wort aus mindestens zwei Buchstaben. Er platziert es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.":{
+    "english":"Forms a word of at least two letters and place it on the board in such a way that a tile covers the field in the middle of the board. Words can be placed horizontally or vertically. Words must not be placed diagonally. All subsequent letters must be placed in a continuous line, either horizontally or vertically."
+  },
+  "Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.":{
+    "english":"The task of the players is to construct words from letter stones with different values and to interpret them in such a way that they are connected with each other according to the type of a crossword puzzle. Each player must strive to achieve the highest possible score by making words out of the available letters, the letter value of which, in combination with the bonus fields of the game plan, results in the highest possible score. The total score that can be achieved in a game is about 400 to 800 and more, depending on the players' ability to play."
+  },
+  "Felder für Buchstabenprämien":{
+    "english":"Letter Award Fields"
+  },
+  "Wird ein Buchstabe auf ein hellblaues Feld gesetzt, so verdoppelt sich sein Wert. Wird ein Buchstabe auf ein dunkelblaues Feld gesetzt, so verdreifacht sich sein Wert.":{
+    "english":"If a letter is placed on a light blue field, its value doubles. If a letter is placed on a dark blue field, its value triples."
+  },
+  "Felder für Wortprämien":{
+    "english":"Word Premium Fields"
+  },
+  "Wird ein Buchstabe auf ein hellrotes Feld gesetzt, so verdoppelt sich der Wert des gesamten Wortes. Wird ein Buchstabe auf ein dunkelrotes Feld gesetzt, so verdreifacht sich der Wert des gesamten Wortes.":{
+    "english":"If a letter is placed on a light red field, the value of the entire word doubles. If a letter is placed on a dark red field, the value of the entire word is tripled."
+  },
+  "Austausch von Buchstabensteinen":{
+    "english":"Replacement of letter stones"
+  },
+  "Ein Spieler hat die Möglichkeit einen oder all seine Buchstabensteine auszutauschen, wenn er an der Reihe ist. Er legt die Buchstabensteine mit dem Buchstaben nach unten zur Seite und zieht die gleiche Anzahl neuer Buchstabensteine aus dem Beutel. Die zur Seite gelegten Buchstabensteine werden dann in den Beutel gelegt und mit den anderen Steinen gemischt. Er muss diese Runde dann aussetzen.":{
+    "english":"A player has the possibility to replace one or all of his letter stones when it is his turn. He puts the letter stones with the letter down to the side and pulls the same number of new letter stones out of the bag. The letter stones placed to the side are then placed in the bag and mixed with the other stones. He must then suspend this round."
   }
 };
